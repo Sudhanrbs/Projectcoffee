22af87eb8c7dd415134e3bd701b242fe815cdd88
Sudhanrbs <161286168+Sudhanrbs@users.noreply.github.com>
2024-04-25 11:01:33 +0530

    some change

--- src/Components/Assets/Reg.jsx
+++ src/Components/Assets/Reg.jsx
@@ -7,31 +7,51 @@ import { useNavigate } from 'react-router-dom';
 import { Footer } from './Footer';
 import axios from 'axios';
 
-export const Reg = () =>{
-  const [roleid]=useState(1);
+export default function Reg() {
+  
 
-  const[reguser, updateRegUser] = useState({
-    username : '',
-    email : '',
-    mobile : '',
-    password : ''
+//   const[reguser, updateRegUser] = useState({
+//     username : '',
+//     email : '',
+//     mobile : '',
+//     password : ''
     
+// })
+
+//    const inputchange = (e)=>{
+//     const {username, value} = e.target;
+//     updateRegUser({  [username] : value});
+//     }
+
+    
+//     const addandsubmit  =async ()=>{
+        
+//       await axios.post(`http://localhost:8383/user/adduser${roleid}`,reguser)
+//     }
+
+const[reguser, updateRegUser] = useState({
+  username : '',
+  email : '',
+  password : '',
+  mobile : '',
+  
 })
 
-   const inputchange = (e)=>{
-    const {username, value} = e.target;
-    updateRegUser({  [username] : value});
-    }
+const inputchange = (e)=>{
+  // console.log("e===============",e);
+  const {name, value} = e.target;
+  updateRegUser({...reguser, [name] : value});
+  console.log(reguser);
+}
+const addandsubmit = ()=>{
+   axios.post(`http://localhost:8383/user/adduser`,reguser)
+  // axios.post(`http://localhost:8099/user/createData`,reguser)
+  
+}
 
-    
-    const addandsubmit  =async ()=>{
-        
-      await axios.post(`http://localhost:8383/user/adduser${roleid}`,reguser)
-    }
-
-const navi = useNavigate()
-const changelogin = () => {
-    navi("/login")}
+// const navi = useNavigate()
+// const changelogin = () => {
+//     navi("/login")}
 
 //     const na = useNavigate()
 //     const change = () => {
@@ -48,26 +68,26 @@ const changelogin = () => {
         <div className='text'>Register Now</div>
       <Form.Group   >
       <Form.Label className='textuser'>User name</Form.Label>
-        <Form.Control type="text" placeholder="Enter user name" value={reguser.username} onChange={inputchange} />
+        <Form.Control type="text" placeholder="Enter user name" name='username' value={reguser.username} onChange={inputchange} />
         
         <Form.Label  className='textuser' controlId="formBasicEmail">Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email"  value={reguser.email} onChange={inputchange}  />
+        <Form.Control type="email" placeholder="Enter email" name='email'   value={reguser.email} onChange={inputchange}  />
        
      
 
       <Form.Label  className='textuser' >Mobile No</Form.Label>
-        <Form.Control type="text" placeholder="Enter Mobile"  value={reguser.mobile} onChange={inputchange}  />
+        <Form.Control type="text" placeholder="Enter Mobile" name='mobile' value={reguser.mobile} onChange={inputchange}  />
      
       
 
         <Form.Label  className='textuser'>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={reguser.password} onChange={inputchange} />
+        <Form.Control type="password" placeholder="Password" name='password' value={reguser.password} onChange={inputchange} />
     
       <div className='buttonlogin'>
-        <Button className='button1' type="submit"   >
+        {/* <Button className='button1' type="submit"   >
         Log in
-      </Button>
-      <Button className='button2' type="submit" onChange={addandsubmit} >
+      </Button> */}
+      <Button className='button2' type="submit" onClick={addandsubmit} >
          Sign up
       </Button>
       
@@ -82,4 +102,4 @@ const changelogin = () => {
   );
 }
 
-export default Reg;
+// export default Reg;
